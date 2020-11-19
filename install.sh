2826bb46c37f2fa1a83e244593e9bf8c83b90e68
*ERR0R* <59941931+Err0r-ICA@users.noreply.github.com>
2020-11-19 18:01:32 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,20 @@
+
+
+clear
+
+apt update
+apt upgrade
+chmod +x /data/data/com.termux/files/home/Venomsploit/cor6e/run
+chmod +x /data/data/com.termux/files/home/Venomsploit/core/run2
+chmod +x /data/data/com.termux/files/home/Venomsploit/Venomsploit
+apt install python2
+apt install wget curl
+apt install perl
+apt install ruby
+gem install lolcat
+pip install requests request future jq mechanize greenlet
+pip2 install requests request future mechanize
+pip install colorama
+pip2 install colorama
+apt install metasploit
+apt install php
